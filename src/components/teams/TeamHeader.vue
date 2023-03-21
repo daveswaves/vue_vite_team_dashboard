528a82cb843a1d4bba7acf5fe453cc4f3de804cf
David <daveswaves.2008@gmail.com>
2023-03-21 10:26:22 +0000

    Add TeamView component to App.vue

---
+++ src/components/teams/TeamHeader.vue
@@ -0,0 +1,24 @@
+<script setup>
+defineProps({
+   team: Object
+});
+</script>
+
+<template>
+   <header class="flex justify-between">
+      <div>
+         <button class="
+         bg-green-500
+         hover:bg-green-400
+         text-white
+         px-4 py-2
+         rounded
+         disabled:bg-gray-400"
+         :disabled="team.members.length == team.spots">Add Member ({{ team.spots - team.members.length }} spots left)</button>
+      </div>
+
+      <div>
+         <h3>{{ team.name }} Team</h3>
+      </div>
+   </header>
+</template>
---
+++ src/components/teams/TeamMember.vue
@@ -0,0 +1,28 @@
+<script setup>
+defineProps({
+   name: String,
+   email: String,
+   status: String
+})
+</script>
+
+<template>
+   <tr class="bg-gray-100 px-12">
+      <td class="text-xl text-black font-medium flex items-center gap-x-4 px-6 py-4">
+         <img :src="`https://i.pravatar.cc/50?u=${email}`" alt="" class="rounded-xl">
+         {{ name }}
+      </td>
+
+      <td class="text-gray-500 px-6 py-4">{{ email }}</td>
+      
+      <td class="px-6 py-4">
+         <button class="text-green-600 text-xl">{{ status }}</button>
+      </td>
+   </tr>
+</template>
+
+<style scoped>
+td {
+   background: #aaa;
+}
+</style>
